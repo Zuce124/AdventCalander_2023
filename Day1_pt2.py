f8c28fb72fc73049977dacf67444388982d57676
Zuce124 <56743171+Zuce124@users.noreply.github.com>
2023-12-04 19:07:04 -0800

    day 2

--- Day1_pt2.py
+++ Day1_pt2.py
@@ -9,13 +9,20 @@ data = open(input_path,'r')
 Content = data.read()
 lines = Content.split("\n")
 
+#indexes
 nums = {'0', '1', '2', '3', '4',
         '5', '6', '7', '8', '9'}
+nums_w = {'zero': 'z0o', 'one': 'o1e', 'two': 't2o', 'three': 't3e', 'four': 'f4r', 'five': 'f5e', 'six': 's6x', 'seven': 's7n',
+          'eight': 'e8t', 'nine': 'n9e'}
+keys = nums_w.keys()
 calibrations = []
-check = []
 
 for i in range(len(lines)):
     line = lines[i]
+    #converting number word to character number
+    for key in keys:
+        line = line.replace(key, nums_w[key])
+
     l = 0
     r = len(line)-1
 
---
+++ Day2_pt1.py
@@ -0,0 +1,49 @@
+import os
+
+# Input path
+dir_path = os.path.dirname(os.path.realpath(__file__))
+input_path = dir_path + "\Inputs\Day2"
+
+# Reading input
+data = open(input_path, 'r')
+Content = data.read()
+lines = Content.split("\n")
+
+# maxes
+red_m = 12
+green_m = 13
+blue_m = 14
+
+# game counter
+game_c = 0
+curr = 0
+
+for i in range(len(lines)):
+    checker = True
+
+    trash, need = lines[i].split('Game')
+    curr, games = need.split(":")
+    curr = curr.replace(" ", "")
+
+    if checker == True:
+        for game in games.split(";"):
+            for cubes in game.split(","):
+                cubes = cubes.replace(" ", "")
+                if "red" in cubes:
+                    cubes, trash = cubes.split("red")
+                    if int(cubes) > red_m:
+                        checker = False
+
+                if "blue" in cubes:
+                    cubes, trash = cubes.split("blue")
+                    if int(cubes) > blue_m:
+                        checker = False
+
+                if "green" in cubes:
+                    cubes, trash = cubes.split("green")
+                    if int(cubes) > green_m:
+                        checker = False
+
+        game_c += int(curr)
+
+print(game_c)
